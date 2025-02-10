c908cc5c24ad9aad1b177ba368b66986abd925c0
root <root@Ashok.>
2025-02-10 09:56:22 +0000

    added convergence test

--- sqrt.py
+++ sqrt.py
@@ -1,9 +1,14 @@
-x = 2
+x = 0.002
 s = 1.
 kmax = 10
+tol = 1.0e-14
 for i in range(kmax):
+    s0 = s
     print(f'At itertion {i} the value of s = {s}')
     s = 0.5*(s + x/s)
+    delta_s = s -s0
+    if (abs(delta_s)/x < tol):
+        break
     
 print(s)
     
