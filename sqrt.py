1de0e3c227fc6b699b50d5395bfe83fdcf2918af
root <root@Ashok.>
2025-02-10 10:11:25 +0000

    converted to sqrt2 function

--- sqrt.py
+++ sqrt.py
@@ -1,15 +1,15 @@
-x = 0.002
-s = 1.
-kmax = 10
-tol = 1.0e-14
-for i in range(kmax):
-    s0 = s
-    print(f'At itertion {i} the value of s = {s}')
-    s = 0.5*(s + x/s)
-    delta_s = s -s0
-    if (abs(delta_s)/x < tol):
-        break
-    
-print(s)
-    
+def sqrt2(x):
+    s = 1.
+    kmax = 100
+    tol = 1.0e-14
+    for i in range(kmax):
+        s0 = s
+        print(f'At itertion {i} the value of s = {s:20.15f}')
+        s = 0.5*(s + x/s)
+        delta_s = s -s0
+        if (abs(delta_s)/x < tol):
+           break
+
+    print(f"Finally, the value of s = {s:20.15f}")
+
     
